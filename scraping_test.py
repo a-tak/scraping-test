7b97d5d56ed3bc7dbdc12757313a33cb448b9ae5
A-tak <hoge+git@a-tak.com>
2017-06-21 16:52:39 +0900

    日本語のデコードがまだだけど取れた

--- scraping_test.py
+++ scraping_test.py
@@ -6,10 +6,12 @@ url = "http://www.yamada-denkiweb.com/category/108/001/009/"
 
 r = requests.get(url)
 
+#soup = BeautifulSoup(r.text.encode(r.encoding), "html.parser")
 soup = BeautifulSoup(r.content, "html.parser")
 
 title_tag = soup.find("title")
+urikire = soup.select(".item-wrapper .note")
 
 print(title_tag)
-
-#print(title)
+print(urikire)
+print("end")
