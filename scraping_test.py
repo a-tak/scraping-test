d20a4d8e22fd69c065d4c8082a318dc125e0d8ea
A-tak <hoge+git@a-tak.com>
2017-06-24 16:53:28 +0900

    まずは実験成功。スクレイピングらしき事が出来た

--- scraping_test.py
+++ scraping_test.py
@@ -7,12 +7,9 @@ url = "http://www.yamada-denkiweb.com/category/108/001/009/"
 
 r = requests.get(url)
 
-#soup = BeautifulSoup(r.text.encode(r.encoding), "html.parser")
 soup = BeautifulSoup(r.content, "html.parser")
 
-title_tag = soup.find("title")
-urikire = soup.select(".item-wrapper .note")
+note_elements = soup.select(".item-wrapper .note")
 
-print(title_tag)
-print(urikire)
-print("end")
+for note_element in note_elements:
+    print(note_element.text)
