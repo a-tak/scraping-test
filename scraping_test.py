7f4cccff6fc16bd080cba80fba9bfd15d8c34303
A-tak <hoge+git@a-tak.com>
2017-06-24 19:03:53 +0900

    スクレイピングロジック共通化

--- scraping_test.py
+++ scraping_test.py
@@ -3,6 +3,7 @@
 import requests
 from result_info import ResultInfo
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 class Main():
     def execute(self):
@@ -11,43 +12,14 @@ class Main():
         results.extend(self.get_joshin())
 
         for result_info in results:
-            print("{0},{1},{2},{3}".format(result_info.result, result_info.title, result_info.note, result_info.url))
+            print("{0},{1},{2},{3}".format(result_info.result, result_info.title, result_info.url, result_info.note))
 
-    def get_yamada(self):
+    def get_result(self, urls, target_element, target_str):
         results = []
-        urls = []
-
-        urls.append("http://www.yamada-denkiweb.com/1177992013")
-        urls.append("http://www.yamada-denkiweb.com/1177991016")
 
         for url in urls:
-            result_info = ResultInfo()
-            
-            r = requests.get(url)
-            soup = BeautifulSoup(r.content, "html.parser")
-            result_info.title = soup.select("title")[0].text.strip()
-            if (len(soup.select(".item-notice-block")) != 0):
-                result_info.note = soup.select(".item-notice-block")[0].text.strip()
-            else:
-                result_info.note = "Not Foud"
-            if (result_info.note != "好評につき売り切れました"):
-                result_info.result = True
-            else:
-                result_info.result = False
-            result_info.url = url
+            print("{1}:処理中…{0}".format(url, datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
 
-            results.append(result_info)
-
-        return results
-
-    def get_joshin(self):
-        results = []
-        urls = []
-
-        urls.append("http://joshinweb.jp/game/40519/4902370535709.html")
-        urls.append("http://joshinweb.jp/game/40519/4902370535716.html")
-
-        for url in urls:
             result_info = ResultInfo()
             
             r = requests.get(url)
@@ -55,10 +27,10 @@ class Main():
             result_info.title = soup.select("title")[0].text.strip()
 
             result_info.result = True
-            result_info.note = "Not Foud"
-            for element in soup.select(".fsL"):
+            result_info.note = ""
+            for element in soup.select(target_element):
                 result_info.note = result_info.note + element.text.strip()
-                if (element.text.strip() == "販売休止中です"):
+                if (element.text.strip() == target_str):
                     result_info.result = False
                     break
 
@@ -67,6 +39,26 @@ class Main():
             results.append(result_info)
 
         return results
+       
+
+    def get_yamada(self):
+        results = []
+        urls = []
+
+        urls.append("http://www.yamada-denkiweb.com/1177992013")
+        urls.append("http://www.yamada-denkiweb.com/1177991016")
+
+        return self.get_result(urls, ".item-notice-block", "好評につき売り切れました")
+
+    def get_joshin(self):
+        results = []
+        urls = []
+
+        urls.append("http://joshinweb.jp/game/40519/4902370535709.html")
+        urls.append("http://joshinweb.jp/game/40519/4902370535716.html")
+
+        return self.get_result(urls, ".fsL", "販売休止中です")
+
 if __name__ == "__main__":
     main_obj = Main()
     main_obj.execute()
